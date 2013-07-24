ddcb00ace843992a4cf24552c6c1306349e08417
palmtoy <zgli@corp.netease.com>
2013-07-24 16:45:52 +0800

    interval:100, del monitor registerack_0

--- app/script/pushweb.js
+++ app/script/pushweb.js
@@ -293,7 +293,6 @@ var http = require('http');
                 console.log('data.code = ', data.code);
                 if (data.code === success) {
                 monitor('end','register',2);
-                monitor('incr', 'registerack_0');
                 isRegister = true;
                 bind2('nA8vcgDWYgYiHkKG','1366883049608','xzm0sznKKF1t5EO8bUyc7Oo4Sr0=');
                 // var req = http.get(login_url,function(res){
